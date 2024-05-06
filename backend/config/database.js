4618653c21e33d2bf9155209053512b1c5631f46
1esuoK <imtrituongg@gmail.com>
2024-05-06 20:37:35 +0700

    add .env and change connection string

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URI || 'mongodb://mongodb:27017/Instagram', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb+srv://21521654:<password>@devcluster.d7stdkl.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongoose Connected");
     }).catch((error) => {
