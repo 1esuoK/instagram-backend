285186bc68ae96d395556679e213684576f666b3
1esuoK <imtrituongg@gmail.com>
2024-05-07 08:55:45 +0700

    test redeployment#3

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+
 const connectDatabase = () => {
-    mongoose.connect('mongodb+srv://21521654:<password>@devcluster.d7stdkl.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(`mongodb+srv://21521654:${password}@devcluster.d7stdkl.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongoose Connected");
     }).catch((error) => {
